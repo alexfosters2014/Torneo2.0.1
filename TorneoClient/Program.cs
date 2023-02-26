663c0a9f26f085cab17566c637400941a1daabc8
alexfosters2014 <alexfosters2014@gmail.com>
2023-02-25 21:50:57 -0300

    front end ok

--- TorneoClient/Program.cs
+++ TorneoClient/Program.cs
@@ -1,7 +1,9 @@
 using Blazored.Modal;
 using Blazored.Toast;
+using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.AspNetCore.SignalR.Client;
 using MudBlazor.Services;
 using TorneoClient;
 using TorneoClient.DataService;
@@ -18,6 +20,15 @@ builder.Services.AddMudServices();
 string backendUrlLocal = builder.Configuration.GetValue<string>("DefaultConnectionWebApi");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration.GetValue<string>("DefaultConnectionWebApi")) });
+
+builder.Services.AddSingleton<HubConnection>(sp =>
+{
+    var navigationManager = sp.GetRequiredService<NavigationManager>();
+    return new HubConnectionBuilder()
+      .WithUrl(navigationManager.ToAbsoluteUri($"{builder.Configuration.GetValue<string>(backendUrlLocal)}/partidohubs"))
+      .WithAutomaticReconnect()
+      .Build();
+});
 
 builder.Services.AddScoped<DataServiceEquipo>();
 builder.Services.AddScoped<DataServiceImagen>();
--- TorneoWebApi/Program.cs
+++ TorneoWebApi/Program.cs
@@ -1,8 +1,10 @@
 using BaseDatosContext;
 using Entidades;
+using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.EntityFrameworkCore;
 using Negocio;
 using TorneoWebApi.EndPoints;
+using TorneoWebApi.PartidoHub;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -26,6 +28,13 @@ builder.Services.AddDbContext<TorneoContext>(opciones =>
     opciones.UseSqlServer(builder.Configuration.GetConnectionString("LocalConnection"));
 });
 
+builder.Services.AddSignalR();
+builder.Services.AddResponseCompression(opts =>
+{
+    opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
+        new[] { "application/octet-stream" });
+});
+
 builder.Services.AddScoped<ImageService>();
 builder.Services.AddScoped<EquiposService>();
 builder.Services.AddScoped<JugadoresService>();
@@ -43,12 +52,16 @@ if (app.Environment.IsDevelopment())
 
 app.UseCors("cors");
 app.UseHttpsRedirection();
+app.UseResponseCompression();
 
 //Configuración de Endpoints
 app.MapImageEndpoints();
 app.MapJugadoresEndpoints();
 app.MapEquiposEndpoints();
 app.MapTorneosEndpoints();
+
+app.UseRouting();
+app.UseEndpoints(endpoints => endpoints.MapHub<PartidoHub>("/partidohubs"));
 
 app.Run();
 
